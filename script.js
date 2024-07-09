0fa1551b8c82176c034495e293b2b382df155eff
Belto254 <natsabel14@gmail.com>
2024-07-09 17:20:07 +0300

    Initial commit with website files

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// Add your JavaScript here
-console.log("Welcome to Rain Blockchain!");
+const axios = require('axios');
+
+async function getLivePrices() {
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
+        const bitcoinPrice = response.data.bitcoin.usd;
+        const ethereumPrice = response.data.ethereum.usd;
+        console.log(`Current Bitcoin Price: $${bitcoinPrice}`);
+        console.log(`Current Ethereum Price: $${ethereumPrice}`);
+        document.getElementById('bitcoin-price').innerText = `Current Bitcoin Price: $${bitcoinPrice}`;
+        document.getElementById('ethereum-price').innerText = `Current Ethereum Price: $${ethereumPrice}`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getLivePrices();
